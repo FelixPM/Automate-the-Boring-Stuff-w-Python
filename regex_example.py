8a089a05cf8b65b16ac9d59358178b25f6c51fd9
Felix Perez <perezmarquezfelix@gmail.com>
2016-07-18 23:28:55 -0300

    regex project: email and phones from clipboard v1

--- regex_example.py
+++ regex_example.py
@@ -3,24 +3,42 @@
 """Python regex script to scrap phone numbers and email out of clipboard content
 """
 
-import pyperclip, re
+import pyperclip
+import re
 
-phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?              # area code
-    (\s|-|\.)?                      # separator
-    \d{3}                           # first 3 digits
-    (\s|-|\.)                       # separator
-    \d{4}                           # last 4 digits
-    (\s*(ext|x|ext.)\s*\d{2,5})?    # extension
-    )''', re.VERBOSE)
+phoneRegex = re.compile(
+    r'((\d{3}|\(\d{3}\))?'              # area code
+    '(\s|-|\.)?'                        # separator
+    '(\d{3})'                           # first 3 digits
+    '(\s|-|\.)'                         # separator
+    '(\d{4})'                           # last 4 digits
+    '(\s*(ext|x|ext.)\s*(\d{2,5}))?)')  # extension
+
 
 emailRegex = re.compile(
-    r'[a-zA-Z0-9._%+-]+'
-    '@'
-    '[a-zA-Z0-9.-]+'
-    '(\.[a-zA-Z]{2,4})', re.VERBOSE)
-
-# TODO: Find matches in clipboard text
+    r'([a-zA-Z0-9._%+-]+'   # username
+    '@'                     # @ symbol
+    '[a-zA-Z0-9.-]+'        # domain name
+    '(\.[a-zA-Z]{2,4}))')   # dot something
 
 
-# TODO: Copy results to the clipboard
+# Get text from clipboard
+text = str(pyperclip.paste())
+
+# Search for phone and email matches
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# Copy results to clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard: ')
+    print('\n'.join(matches))
+else:
+    print('No phone or email found')
