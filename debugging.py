dc6f1d397c6287cf7e240ea00e8fd48b13d524be
Felix Perez <perezmarquezfelix@gmail.com>
2016-08-04 22:04:49 -0400

    debugging. chapter 10 part 2 traceback and assertion

--- debugging.py
+++ debugging.py
@@ -1,6 +1,7 @@
 """Tools and techniques for debugging
 """
 
+import traceback
 # raise Exception
 
 
@@ -17,14 +18,32 @@ def box_print(symbol, width, height):
     print(symbol * width)
 
 
-for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
-    try:
-        box_print(sym, w, h)
-    except Exception as err:
-        print('An exception happened: ' + str(err))
+# for sym, w, h in (('*', 4, 4), ('O', 20, 5), ('x', 1, 3), ('ZZ', 3, 3)):
+#     try:
+#         box_print(sym, w, h)
+#     except Exception as err:
+#         print('An exception happened: ' + str(err))
 
 # box_print('**', 5, 5)
 # box_print('+', 1, 3)
 # box_print('-', 3, 2)
-box_print('/', 3, 3)
+# box_print('/', 3, 3)
+
+# tracebacks to file
+try:
+    raise Exception('This is the error message')
+except:
+    errorFile = open('errorInfo.txt', 'a')
+    errorFile.write(traceback.format_exc())
+    errorFile.close()
+    print('Traceback info written to errorInfo.txt')
+
+# assertions sanity checks
+# assert condition, 'error message'
+
+podBayDoorStatus = 'open'
+assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+podBayDoorStatus = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.'
+assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+
 
